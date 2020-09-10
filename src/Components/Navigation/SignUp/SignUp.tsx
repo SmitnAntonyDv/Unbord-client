6d9c8fadc813a27a32f11f6d76ee086232918d5f
SmitnAntonyDv <anton.smit@hotmail.com>
2020-09-10 12:22:41 +0200

    init singUp file + input field

---
+++ src/Components/Navigation/SignUp/SignUp.tsx
@@ -0,0 +1,15 @@
+/*input field items:
+- first Name
+- last Name
+- username [unique constrain - BE]
+- Email [unique constrain - BE]
+- Password 
+- verify password
+
+*/
+
+import React from 'react';
+
+export default function SignUp() {
+  return <div></div>;
+}
