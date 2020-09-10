6ec25731a8f77e79de2e930e2de9eda0d964c129
SmitnAntonyDv <anton.smit@hotmail.com>
2020-09-10 12:49:20 +0200

    basic singup input form

--- src/Components/Navigation/SignUp/SignUp.tsx
+++ src/Components/Navigation/SignUp/SignUp.tsx
@@ -9,7 +9,25 @@
 */
 
 import React from 'react';
+import { SignUpData } from '../../../Types/UserTypes';
 
 export default function SignUp() {
-  return <div></div>;
+  return (
+    <>
+      <form>
+        <label>First Name</label>
+        <input type='text' required placeholder='First Name' />
+        <label>Last Name</label>
+        <input type='text' required placeholder='Last Name' />
+        <label>Username</label>
+        <input type='text' required placeholder='Username' />
+        <label>Email</label>
+        <input type='email' required placeholder='Email' />
+        <label>Password</label>
+        <input type='password' required placeholder='Password' />
+        <label>Verify your password</label>
+        <input type='password' required placeholder='Repeat Password' />
+      </form>
+    </>
+  );
 }
