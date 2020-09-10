2fcc68169a5af98f555931ecf56395815b5d05ba
SmitnAntonyDv <anton.smit@hotmail.com>
2020-09-10 13:35:58 +0200

    added controlled input to signup form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import SignUp from './Components/Navigation/SignUp/SignUp';
 
 //development branch
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <SignUp />
+    </div>
+  );
 }
 
 export default App;
