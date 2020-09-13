86a2f52463523d540f43895d4e074343af90e91c
SmitnAntonyDv <anton.smit@hotmail.com>
2020-09-13 14:28:48 +0200

    created switch route with exact path for homepage

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,5 +1,21 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Homepage() {
-  return <div></div>;
+  /*
+  1) need a logo componennt
+  2) navbar component
+        - with titles:
+            1) How It works
+            2) About us
+            3) Sign Up button
+  3) Try now button (the CTA button)
+  */
+
+  return (
+    <div>
+      <h1>Get out of your Comfort Zone</h1>
+      <h3>Subtitle substitle</h3>
+      <button>TRY NOW</button>
+    </div>
+  );
 }
